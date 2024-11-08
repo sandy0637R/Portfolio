cb4ed2e0c061f007427cff8da2cfccf07a7e87c5
sandy0637R <sandeshraut842@gmail.com>
2024-11-08 03:31:04 -0800

    Updated

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+function carrerGuidance(){
+    window.location="https://github.com/sandy0637R/CareerGuidance.git"
+}
+
+function foodOutlet(){
+    window.location="https://github.com/sandy0637R/FoodOutlet.git"
+}
+
+function ecomApplication(){
+    window.location="https://github.com/sandy0637R/ECOM_Application.git"
+}
+
+function jarvisClone(){
+    window.location="https://github.com/sandy0637R/Jarvis-Website.git"
+}
+
+function calculator(){
+    window.location="https://github.com/sandy0637R/Calculator.git"
+}
+
+function toDoList(){
+    window.location="https://github.com/sandy0637R/TP_RP_05.git"
+}
